d4826d118565f3d9934460732024bb12ba28d7da
Reet Oberoi <reet@umich.edu>
2025-04-25 22:29:24 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -95,3 +95,11 @@ window.addEventListener('resize', checkScreenAndRunPhotoStack);
 document.addEventListener('DOMContentLoaded', () => {
   checkScreenAndRunPhotoStack();
 });
+
+// Footer
+document.addEventListener("DOMContentLoaded", function() {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
